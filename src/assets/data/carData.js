d3eab0435ddb5f86e3fe266e9c3af9f0e59fcb1e
lanre33 <jaruna46@gmail.com>
2022-12-20 00:26:46 +0100

    initial commit

---
+++ src/assets/data/carData.js
@@ -0,0 +1,141 @@
+// import all images from assets/images directory
+import img01 from "../all-images/cars-img/2.png";
+import img02 from "../all-images/cars-img/5.png";
+import img03 from "../all-images/cars-img/1.png";
+import img04 from "../all-images/cars-img/2.png";
+import img05 from "../all-images/cars-img/5.png";
+import img06 from "../all-images/cars-img/4.png";
+import img07 from "../all-images/cars-img/inverter.png";
+import img08 from "../all-images/cars-img/4.png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "CCTV",
+    rating: 112,
+    carName: "CCTV",
+    imgUrl: img01,
+    model: "Model 3",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 2,
+    brand: "Solar",
+    rating: 102,
+    carName: "Solar Inverter",
+    imgUrl: img02,
+    model: "Model-2022",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 3,
+    brand: "Small inverter",
+    rating: 132,
+    carName: "inverter",
+    imgUrl: img03,
+    model: "Model-2022",
+    price: 65,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 4,
+    brand: "CCTV",
+    rating: 102,
+    carName: "CCTV",
+    imgUrl: img04,
+    model: "Model-2022",
+    price: 70,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 5,
+    brand: "Solar",
+    rating: 94,
+    carName: "Solar Inverter",
+    imgUrl: img05,
+    model: "Model-2022",
+    price: 45,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 6,
+    brand: "Solar",
+    rating: 119,
+    carName: "Solar",
+    imgUrl: img06,
+    model: "Model-2022",
+    price: 85,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 7,
+    brand: "inverter",
+    rating: 82,
+    carName: "inverter",
+    imgUrl: img07,
+    model: "Model 3",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 8,
+    brand: "Solar",
+    rating: 52,
+    carName: "Solar",
+    imgUrl: img08,
+    model: "Model 3",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+];
+
+export default carData;
---
+++ src/components/UI/FindCarForm.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../../styles/find-car-form.css";
+import "../../styles/find-car-form.css";
+import { Form, FormGroup } from "reactstrap";
+
+const FindCarForm = () => {
+  return (
+    <Form className="form">
+      <div className=" d-flex align-items-center justify-content-between flex-wrap">
+        <FormGroup className="form__group">
+          <input type="text" placeholder="From address" required />
+        </FormGroup>
+
+        <FormGroup className="form__group">
+          <input type="text" placeholder="To address" required />
+        </FormGroup>
+
+        <FormGroup className="form__group">
+          <button className="btn find__car-btn">Find Items</button>
+        </FormGroup>
+      </div>
+    </Form>
+  );
+};
+
+export default FindCarForm;
